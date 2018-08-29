6d2294ec61e1f80b956581cd8213ad4440e37cf3
Lecostarius <aingram1@hotmail.com>
2018-08-29 21:24:27 +0200

    Version that works with the turbine. Uses serial to control it, which needs to be changed.

--- wespe/wespe.ino
+++ wespe/wespe.ino
@@ -7,32 +7,60 @@ int state = 0;
 int substate = 0;
 
 void heartbeat();
+void startMotor();
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  
   Serial.begin(57600); while(!Serial); // wartet bis Serial da ist
   
   myservo3.attach(3,1000,2000); // attaches the servo on pin 3 to the servo object
   write2servo(us);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
   
   // wait a little for the ESC to boot up
-  for (int i=0; i < 20; i++) {
+  //for (int i=0; i < 20; i++) {
+  //  heartbeat();
+  //  delay(100);
+  //}
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("OK\n");
+  while (Serial.available() <= 0) {
     heartbeat();
-    delay(100);
   }
+  Serial.println("START\n");
+  startMotor();
  
 }
 
-void write2servo(int us) {
-  if (us > 900 && us < 2100) {myservo3.writeMicroseconds(us); Serial.println(us);}
+void startMotor() {
+  us=1310; write2servo(us); heartbeat();
+  us=1330; write2servo(us); heartbeat();
+  us=1350; write2servo(us); heartbeat();
+  us=1310; write2servo(us); heartbeat();
+  us=1330; write2servo(us); heartbeat();
+  us=1150; write2servo(us); heartbeat();
 }
+
+void write2servo(int us) {
+  if (us > 900 && us < 2100) {
+    myservo3.writeMicroseconds(us); 
+    //Serial.println(us);
+  }
+}
+
+
 void loop() {
-
-
   //
   // show we are alive
   heartbeat();
-
+  us--;
+  write2servo(us);
+  
+#ifdef UNDEFINED
   switch(state) {
     case 0: us = 1310; write2servo(us); state=1; substate=0; break;
     case 1: {
@@ -54,6 +82,9 @@ void loop() {
     state = 0;
     // retry for ever...            
   }
+#endif
+
+  //startMotor();
   
   // OK, we are done, wait
   delay(500);
