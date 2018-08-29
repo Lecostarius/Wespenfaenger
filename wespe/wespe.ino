b95e688198dd057c8e0abab38f129874126b866d
Lecostarius <aingram1@hotmail.com>
2018-08-29 14:12:50 +0200

    Added initial revision, based on blink sketch

---
+++ wespe/wespe.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  // check for inputs:
+  //
+  // show we are alive
+  heartbeat();
+
+  // OK, we are done, wait
+  delay(500);
+}
+
+// flickers the LED briefly
+void heartbeat() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+}
+
